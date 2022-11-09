c8413cfb159304c0e2f9de377d7b856b2d7ea95a
Daniel Davidson <1096508+patefacio@users.noreply.github.com>
2022-11-09 09:30:14 -0600

    Add visibility to Enum

--- src/main/kotlin/kgen/rust/Enum.kt
+++ src/main/kotlin/kgen/rust/Enum.kt
@@ -1,24 +1,28 @@
 package kgen.rust
 
-import kgen.Identifier
-import kgen.commentTriple
-import kgen.indent
-import kgen.missingDoc
+import kgen.*
 
 data class Enum(
     val nameId: String,
     val doc: String = missingDoc(nameId, "Enum"),
     val values: List<EnumValue>,
+    val visibility: Visibility = Visibility.None,
 ) : Identifier(nameId), AsRust {
 
-    constructor(nameId: String, doc: String = missingDoc(nameId, "Enum"), vararg values: EnumValue) : this(
+    constructor(
+        nameId: String,
+        doc: String = missingDoc(nameId, "Enum"),
+        vararg values: EnumValue,
+        visibility: Visibility
+    ) : this(
         nameId,
         doc,
-        values.toList()
+        values.toList(),
+        visibility
     )
 
     override val asRust: String
-        get() = "${commentTriple(doc)}\nenum ${id.capCamel} {\n${
+        get() = "${commentTriple(doc)}\n${trailingText(visibility.asRust)}enum ${id.capCamel} {\n${
             indent(values.joinToString(",\n") { it.asRust })
         }\n}"
 }
