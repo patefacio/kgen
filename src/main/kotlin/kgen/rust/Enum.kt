c8ec9eedf3d445c27b203db9194a202d60af58dc
Daniel Davidson <1096508+patefacio@users.noreply.github.com>
2022-11-12 17:54:12 -0600

    Default Enum to Pub, + attrs

--- src/main/kotlin/kgen/rust/Enum.kt
+++ src/main/kotlin/kgen/rust/Enum.kt
@@ -6,24 +6,30 @@ data class Enum(
     val nameId: String,
     val doc: String = missingDoc(nameId, "Enum"),
     val values: List<EnumValue>,
-    val visibility: Visibility = Visibility.None,
+    val visibility: Visibility = Visibility.Pub,
+    val attrs: AttrList = AttrList()
 ) : Identifier(nameId), AsRust {
 
     constructor(
         nameId: String,
         doc: String = missingDoc(nameId, "Enum"),
         vararg values: EnumValue,
-        visibility: Visibility
+        visibility: Visibility,
+        attrs: AttrList = AttrList()
     ) : this(
         nameId,
         doc,
         values.toList(),
-        visibility
+        visibility,
+        attrs
     )
 
     override val asRust: String
-        get() = "${commentTriple(doc)}\n${trailingText(visibility.asRust)}enum ${id.capCamel} {\n${
-            indent(values.joinToString(",\n") { it.asRust })
-        }\n}"
+        get() = listOfNotNull(
+            attrs.asRust,
+            "${commentTriple(doc)}\n${trailingText(visibility.asRust)}enum ${id.capCamel} {\n${
+                indent(values.joinToString(",\n") { it.asRust })
+            }\n}"
+        ).joinNonEmpty()
 }
 
